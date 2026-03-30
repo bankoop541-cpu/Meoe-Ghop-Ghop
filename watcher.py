dcadf1e11f753ae7c227fc1d9b86e5093aaba54a
Ansh Rai <90785663+ansh5025@users.noreply.github.com>
2026-03-30 05:49:49 +0530

    Change API token and admin ID in watcher.py
    Updated API token and admin ID for the bot.

--- watcher.py
+++ watcher.py
@@ -4,8 +4,8 @@ import logging
 from aiogram import Bot
 import asyncio
 
-API_TOKEN = '7262217536:AAHX6ROfNRk1ZH9ZcwokcwvyR4jFtk3fKP8'
-ADMIN_ID = '1065510487'
+API_TOKEN = '8691273219:AAGETZiBigH-pGBgG91vRSaoKDsm3aopWFM'
+ADMIN_ID = '8400776382'
 MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
 
